f0b89257112f9d737775616a4adfffaaf37827d3
SND\mpoindexter_cp <SND\mpoindexter_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-05-28 23:09:24 +0000

    Add web page debug logger
    git-svn-id: https://projectstarlight.svn.codeplex.com/svn@20138 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ trunk/Managed/ProjectStarlight/StarlightLib/WebPageDebugLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Browser;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Starlight.Lib
+{
+    public class WebPageDebugLogger : Starlight.ASF.IDebugLogger
+    {
+        private HtmlElement element;
+        private Dispatcher dispatcher;
+
+        public WebPageDebugLogger(HtmlElement element)
+        {
+            this.element = element;
+            this.dispatcher = element.Dispatcher;
+        }
+
+        public void WriteDebug(string message)
+        {
+            dispatcher.BeginInvoke(delegate()
+            {
+                string html = element.GetAttribute("innerHTML");
+                html += "<div><pre>" + message + "</pre></div>";
+                element.SetAttribute("innerHTML", html);
+            });
+        }
+    }
+}
